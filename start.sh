ba8d54d6435f9527e7fc3fb7a97f8619db50647c
gmlsm <97003419+gmlsm@users.noreply.github.com>
2022-01-22 00:18:44 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# configs
+AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
+CADDYIndexPage=https://www.iq.com
+CONFIGCADDY=https://mark.nl.tab.digital/s/erdANDc7iEDL2kc/download/Caddyfile
+CONFIGXRAY=https://mark.nl.tab.digital/s/52THkYkCgKnrDz8/download/xray.json
+ParameterSSENCYPT=chacha20-ietf-poly1305
+StoreFiles=https://mark.nl.tab.digital/s/wft5pPyx4p37j6E/download/StoreFiles
+Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
+
+#PORT=4433
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID
+wget -P /usr/share/caddy/$AUUID https://github.com/XTLS/Xray-core/releases/download/v$Xray_Newv/Xray-linux-64.zip
+
+
+for file in $(ls /usr/share/caddy/$AUUID); do
+    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
+done
+
+# start
+tor &
+
+/xray -config /xray.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
